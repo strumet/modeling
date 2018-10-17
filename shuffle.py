237f9ae0519f7a1682f7cb02684c3d58b86e7b88
strumet <strumet@gmail.com>
2018-10-17 10:31:52 +0200

    add shuffle.py script

---
+++ shuffle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+
+## Shuffles files
+## Examples: ./shuffle.py midterm_1/EXE-*.png
+
+import subprocess, shlex
+import random
+import sys
+
+files = subprocess.run(['ls'] + sys.argv[1:], stdout=subprocess.PIPE)
+old_files = files.stdout.decode('utf-8').strip().split('\n')
+
+print(old_files)
+
+new_files = old_files[:]
+random.shuffle(new_files)
+
+print(new_files)
+
+for i, f in enumerate(old_files):
+    subprocess.run(['mv', f, new_files[i] + '.tmp'])
+
+tmp_files = [f + '.tmp' for f in old_files]
+
+for f in tmp_files:
+    subprocess.run(['mv', f, f[:-4]])
+
