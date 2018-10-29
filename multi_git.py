9d685fd4cb47f5e8ff81ed22ee3f36bad43f04f5
strumet <strumet@gmail.com>
2018-10-29 15:56:33 +0100

    implement prepare_mail() in multigit.py

--- multi_git.py
+++ multi_git.py
@@ -18,20 +18,41 @@ import subprocess, shlex
 import re
 
 act_dict = {
-        'activities/0/': re.compile('^\d{6}\.(jpg|png|jpeg)$', re.IGNORECASE),
-        'activities/1/': re.compile('^\d{6}[-_]0[1-3][ABC]\.obj$', re.IGNORECASE),
-        'activities/2/': re.compile('^\d{6}[-_]\d\d(-LP)?\.obj$', re.IGNORECASE),
-        'activities/3/': re.compile('^\d{6}[-_][ABCF](-LP)?\.obj$', re.IGNORECASE),
-        'exercises/': re.compile('^\d{6}[-_]\d\d(-LP)?\.obj$', re.IGNORECASE),
-        'midterm_1/': re.compile('^\d{6}[-_]\d\d(-LP)?\.obj$', re.IGNORECASE),
+        'activities/0/': {
+            'regex': re.compile('^\d{6}\.(jpg|png|jpeg)$', re.IGNORECASE),
+            'url': 'https://github.com/strumet/modeling/tree/master/activities/0#activity-0'},
+        'activities/1/': {
+            'regex': re.compile('^\d{6}[-_]0[1-3][ABC]\.obj$', re.IGNORECASE),
+            'url': 'https://github.com/strumet/modeling/tree/master/activities/1#activity-1'},
+        'activities/2/': {
+            'regex': re.compile('^\d{6}[-_]\d\d(-LP)?\.obj$', re.IGNORECASE),
+            'url': 'https://github.com/strumet/modeling/tree/master/activities/2#activity-2'},
+        'activities/3/': {
+            'regex': re.compile('^\d{6}[-_][ABCF](-LP)?\.obj$', re.IGNORECASE),
+            'url': 'https://github.com/strumet/modeling/tree/master/activities/3#activity-3'},
+        'exercises/': {
+            'regex': re.compile('^\d{6}[-_]\d\d(-LP)?\.obj$', re.IGNORECASE),
+            'url': 'https://github.com/strumet/modeling/tree/master/exercises#exercises'},
+        'midterm_1/': {
+            'regex': re.compile('^\d{6}[-_]\d\d(-LP)?\.obj$', re.IGNORECASE),
+            'url': 'https://github.com/strumet/modeling/tree/master/midterm_1#first-midterm'},
         }
 
 reports = {}
+mails = {}
 
+id_re = re.compile('/(\d{6})')
 get_range = lambda li: [val for val in range(li[0], li[1]+1)]
 get_format = lambda x: get_range(list(map(int, x.split('-')))) if '-' in x else map(int, x.split(',')) 
 check_file = lambda x: 'git diff --stat --name-only HEAD...origin/pr/' + str(x)
 get_path_index = lambda x: x.rindex('/') + 1 if '/' in x else 1
+get_path_url = lambda p: " pubblicata all'indirizzo " + act_dict[p]['url'] if p in act_dict else '.'
+mail_body = lambda id_no, wrong_files, path: "Ciao " + STUD_DICT[id_no][1].capitalize() + ",\n" + \
+        "nella tua consegna sono presenti errori formali relativi ai file:\n\n" + \
+        ('').join(['* ' + f + '\n' for f in wrong_files]) + "\n" + \
+       "Verifica che il nome, il formato e le cartelle dei file inviati corrispondano a " + \
+       "quanto indicato nella pagina di istruzioni" + get_path_url(path) + \
+       "\nUn saluto,\nmf"
 commands = {
         'diff': lambda x: 'git diff --stat HEAD...origin/pr/' + str(x),
         'merge': lambda x: 'git merge origin/pr/' + str(x)
@@ -41,31 +62,46 @@ CMD = sys.argv[1]
 RAW_PR = sys.argv[2:]
 NESTED_PR = [get_format(i) for i in RAW_PR]
 PR = [item for sublist in NESTED_PR for item in sublist]
+STUDENTS_FILE = '/home/mf/Documents/uni/strumod/documents/status.csv'
+STUD_LIST = [l.split(';') for l in open(STUDENTS_FILE, encoding="ISO-8859-1").read().split('\n')]
+STUD_DICT = {l[0]: l[1:4] for l in STUD_LIST if len(l) > 3 and re.match('\d{6}', l[0])}
 
 
-for i in PR:
-    print(commands[CMD](i))
-    cmd = subprocess.run(shlex.split(commands[CMD](i)), stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-            universal_newlines=True)
-    print(cmd.stdout)
-    if len(cmd.stderr) > 0:
-        print(cmd.stderr)
-    ## Check formal correctness
-    if CMD == 'diff':
-        reports[str(i)] = []
-        check = subprocess.run(shlex.split(check_file(i)), stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+def main():
+    for i in PR:
+        print(commands[CMD](i))
+        cmd = subprocess.run(shlex.split(commands[CMD](i)), stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 universal_newlines=True)
-        files = check.stdout.strip().split('\n')
-        for f in files:
-            path = f[:get_path_index(f)]
-            filename = f[get_path_index(f):].replace('_', '-')
-            if path not in act_dict or not act_dict[path].search(filename):
-                reports[str(i)].append(f)
-        print()
+        print(cmd.stdout)
+        if len(cmd.stderr) > 0:
+            print(cmd.stderr)
+        ## Check formal correctness
+        if CMD == 'diff':
+            reports[str(i)] = []
+            check = subprocess.run(shlex.split(check_file(i)), stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                    universal_newlines=True)
+            files = check.stdout.strip().split('\n')
+            for f in files:
+                path = f[:get_path_index(f)]
+                filename = f[get_path_index(f):].replace('_', '-')
+                if len(filename) > 0 and (path not in act_dict or not act_dict[path]['regex'].search(filename)):
+                    reports[str(i)].append(f)
+            print()
 
-for d in reports:
-    if len(reports[d]) > 0:
-        print(d, ':')
-        for f in reports[d]:
-            print('\t', f)
+    if CMD == 'diff':
+    ## Create report of wrong files
+        for d in reports:
+            if len(reports[d]) > 0:
+                id_no = id_re.search(reports[d][0]).group(1)
+                print(d, ':')
+                for f in reports[d]:
+                    print('\t', f)
+                mails[d] = mail_body(id_no, reports[d], path)
+    ## Print out the mail text to send
+        mail = input('\nPrepare mail?\n(y/n): ').lower()
+        if mail == 'y':
+            for m in mails:
+                print('\n' + m + ':\n' + mails[m])
 
+
+main()
