a553459148c28e0f2a2942d12a4b4c7ab82212fc
strumet <strumet@gmail.com>
2018-10-29 19:02:41 +0100

    fix mail asking if no wrong files

--- multi_git.py
+++ multi_git.py
@@ -98,10 +98,11 @@ def main():
                     print('\t', f)
                 mails[d] = mail_body(id_no, reports[d], path)
     ## Print out the mail text to send
-        mail = input('\nPrepare mail?\n(y/n): ').lower()
-        if mail == 'y':
-            for m in mails:
-                print('\n' + m + ':\n' + mails[m])
+        if len(reports[d]) > 0:
+            mail = input('\nPrepare mail?\n(y/n): ').lower()
+            if mail == 'y':
+                for m in mails:
+                    print('\n' + m + ':\n' + mails[m])
 
 
 main()
